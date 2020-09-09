66213b493be9e6b7ae72b32b4d9bce2eed34aca8
sakosandri <xhivini@gmail.com>
2020-09-09 15:30:42 +0200

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'waze-backend';
+
 }
