5cdf01f8161cd0c3ce1594ea6cc15577e67f6253
sakosandri <xhivini@gmail.com>
2020-09-10 21:03:31 +0200

    modules

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,15 +1,16 @@
+
 import { AdminUsersComponent } from '../../pages/admin-users/admin-users.component';
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { UsersComponent } from '../../pages/users/users.component';
 import { TrackingLocationComponent } from '../../pages/tracking-location/tracking-location.component';
 import { NotificationsComponent } from '../../pages/notifications/notifications.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
+    // tslint:disable-next-line: max-line-length
+    { path: 'user-profile',   loadChildren: () => import('./../../pages/user-profile/user-profile.module').then((m) => m.UserProfileModule) },
     { path: 'users',          component: UsersComponent },
     { path: 'tracking-location',           component: TrackingLocationComponent },
     { path: 'notifications',  component: NotificationsComponent },
